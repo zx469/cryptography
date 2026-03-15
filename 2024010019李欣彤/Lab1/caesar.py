2a98bf57d2d69b598b0a5e04a6b8e3729518dadb
李欣彤 <2024010019@yxnu.edu.cn>
2026-03-15 23:39:14 +0800

    done

--- 2024010019李欣彤/Lab1/caesar.py
+++ 2024010019李欣彤/Lab1/caesar.py
@@ -1,18 +1,16 @@
+# Lab1：穷举法破译凯撒密码
+# 功能：对密文进行所有1~25位的凯撒解密，并输出
+
 cipher = "NUFECMWBYUJMBIQGYNBYWIXY"
 
 for k in range(1, 26):
-    result = ""
-    for char in cipher:
-       
-        if 'A' <= char <= 'Z':
-            
-            new_ord = ord(char) - k
-           
-            if new_ord < ord('A'):
-                new_ord += 26
-            result += chr(new_ord)
+    plain = ""
+    for c in cipher:
+        if 'A' <= c <= 'Z':
+            # 解密：往前移k位，循环26个字母
+            num = ord(c) - ord('A')
+            num = (num - k) % 26
+            plain += chr(num + ord('A'))
         else:
-            
-            result += char
-    
-    print(f"k={k:<2d} : {result}")
+            plain += c
+    print(f"k={k:<2d} : {plain}")
